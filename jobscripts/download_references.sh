b9b88f4c5c5de061caa28da3bf99ed45b8636bee
Eric Coissac <Eric.coissac@metabarcoding.org>
2026-03-04 18:01:15 +0100

    add a jobscript to download all reference sequences

---
+++ jobscripts/download_references.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#OAR -n download_partial_gb
+#OAR -l /nodes=1/core=10,walltime=48:00:00
+#OAR --project phyloalps
+#OAR -O %jobname%.%jobid%.stdout
+#OAR -E %jobname%.%jobid%.stderr
+
+# This job download a few division of genbank useful
+# to setup genome cleaning by tagging :
+#   - bacterial,
+#   - human,
+#   - fungal
+# sequences
+#
+# Use the commands to run the job
+#
+# cd /bettik/LECA/home/pan/G15X/genbank
+# oarsub -S ./genbank_download.sh
+
+G15x_HOME=/bettik/LECA/home/pan/G15X
+
+cd ${G15x_HOME}/docker
+make download_references
